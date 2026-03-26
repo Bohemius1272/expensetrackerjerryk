2cd7ebc95f6c3cd485f40711e7ab8206e2799eef
Bohemius1272 <JerryKnyazhevsky@gmail.com>
2026-03-26 12:14:20 -0400

    commit numba three

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -12,18 +12,28 @@ class Expenses extends StatefulWidget{
 }
 class _ExpensesState extends State<Expenses>{
   final List<Expense> _registeredExpenses = [
-    Expense(title: "Mixue", 
+
+    Expense(
+    title: "Mixue", 
     amount: 25.00, 
     date: DateTime.now(), 
     category: Category.food
     ),
 
-    Expense(title: "Plane ticket to Moldova", 
+    Expense(
+    title: "Plane ticket to Moldova", 
     amount: 15.25, 
     date: DateTime.now(), 
     category: Category.travel
     ),
 
+     Expense(
+    title: "Tung Tung Tung Sahur body pillow", 
+    amount: 6.70, 
+    date: DateTime.now(), 
+    category: Category.leisure
+    ),
+    
   ];
   @override
   Widget build(BuildContext context) {
@@ -31,8 +41,8 @@ class _ExpensesState extends State<Expenses>{
       body: Column(
         children: [
           Text("CHART GOES HERE"),
-          SizedBox(height:30),
-          ExpensesList(expenses: _registeredExpenses),
+          SizedBox(height:300,
+          child: ExpensesList(expenses:_registeredExpenses)),
         ],
     ),
     );
