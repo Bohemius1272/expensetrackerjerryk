76cbe39963e1e76333408eae6bbd303cc24d47fd
Bohemius1272 <JerryKnyazhevsky@gmail.com>
2026-04-14 12:13:12 -0400

    started theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,37 @@
 import 'package:expensetrackerjerryk/expenses.dart';
 import 'package:flutter/material.dart';
 
+ColorScheme kColorScheme = ColorScheme.fromSeed
+(seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses()
-  ));
+  runApp(
+    MaterialApp(
+    theme: ThemeData(
+      textTheme: ThemeData().textTheme.copyWith(
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.bold, 
+          color: kColorScheme.onSecondaryContainer,
+          fontSize: 20,
+        ),
+      ),
+      colorScheme: kColorScheme, 
+      appBarTheme: AppBarTheme(
+        backgroundColor: kColorScheme.onPrimaryContainer, 
+        foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData(
+          color: kColorScheme.secondaryContainer, 
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+    ),
+    home: Expenses(),
+  ),
+  );
 }
