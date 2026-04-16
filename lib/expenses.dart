48cc8bb5359812801652bb0effd92dcbf6228013
Bohemius1272 <JerryKnyazhevsky@gmail.com>
2026-04-16 12:15:56 -0400

    4/16/2026 commit

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
+import 'package:expensetrackerjerryk/widgets/chart/chart.dart';
 import 'package:expensetrackerjerryk/widgets/expenses_list/expenses_list.dart';
+import 'package:expensetrackerjerryk/widgets/expenses_list/expense_item.dart';
 import 'package:expensetrackerjerryk/models/expense.dart';
 import 'package:expensetrackerjerryk/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -85,7 +87,7 @@ class _ExpensesState extends State<Expenses>{
       ),
       body: Column(
         children: [
-          Text("CHART GOES HERE"),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
     ),
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 ColorScheme kColorScheme = ColorScheme.fromSeed
 (seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+  brightness: Brightness.dark,
+);
 
 void main() {
   runApp(
@@ -31,6 +35,19 @@ void main() {
           ),
         ),
     ),
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: CardThemeData(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+        ),
+      ),
+    ),
+      themeMode: ThemeMode.system,
     home: Expenses(),
   ),
   );
